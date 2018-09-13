9d585128d1262745f769ce7b82bd7db1da3c1889
engreal <732261732@qq.com>
2018-09-13 22:30:39 +0800

    Create 2.7.scm

---
+++ chapter2/2.7.scm
@@ -0,0 +1,38 @@
+(define (add-interval x y)
+    (make-interval  (+ (lower-bound x) (lower-bound y))
+                    (+ (upper-bound x) (upper-bound y))))
+
+(define (mul-interval x y)
+    (let (  (p1 (* (lower-bound x) (lower-bound y)))
+            (p2 (* (lower-bound x) (upper-bound y)))
+            (p3 (* (upper-bound x) (lower-bound y)))
+            (p4 (* (upper-bound x) (upper-bound y))))
+        (make-interval  (min p1 p2 p3 p4)
+                        (max p1 p2 p3 p4))))
+
+(define (div-interval x y)
+    (if (< (lower-bound y) 0.0 (upper-bound y))
+        `error
+        (mul-interval   x 
+            (make-interval  (/ 1.0 (upper-bound y))
+                            (/ 1.0 (lower-bound y))))))
+
+(define (sub-interval x y)
+    (make-interval  (- (lower-bound x) (upper-bound y))
+                    (- (upper-bound x) (lower-bound y))))
+
+(define (make-interval a b)
+    (cons a b))
+
+(define (upper-bound x)
+    (cdr x))
+
+(define (lower-bound x)
+    (car x))
+
+(define (make-center-width c w)
+    (make-interval (- c w) (+ c w)))
+
+(define (equal-interval x y)
+    (and    (= (lower-bound x) (lower-bound y))
+            (= (upper-bound x) (upper-bound y))))
